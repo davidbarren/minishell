cced19c055dba9f232c685f60b38b5ac3cc41be6
Patrik Lang <plang@c1r4p10.hive.fi>
2024-04-12 19:15:24 +0300

    added trim function

---
+++ src/trim_input.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   trim_input.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plang <plang@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/12 18:24:57 by plang             #+#    #+#             */
+/*   Updated: 2024/04/12 19:13:52 by plang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+char	*trim_input(char *input, char c)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	k = ft_strlen(input) - 1;
+	if (input[i] == c)
+	{
+		while (input[i] == c)
+			i++;
+	}
+	if (input[k] == c)
+	{
+		while (input[k] == c)
+			k--;
+	}	
+	str = malloc((k - i + 2) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (i <= k)
+		str[j++] = input[i++];
+	str[j] = '\0';
+	return (str);
+}
